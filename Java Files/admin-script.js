ecce0f34a6d0a5bf5d2bd0ddb0745b374f330ff6
Hector Vazquez <57381072+HectorVA@users.noreply.github.com>
2024-04-16 09:04:38 -0700

    modified:   CSS Files/stock-trading-styles.css modified:   Java Files/admin-script.js

--- Java Files/admin-script.js
+++ Java Files/admin-script.js
@@ -5,6 +5,33 @@ function toggleMenu() {
     navbar.classList.toggle('showMenu'); // This toggles the class on and off
 }
 
+function createNewStock(companyName, stockTicker, volume, initialPrice) {
+    // Construct the data object you will send to the server
+    const stockData = {
+        Symbol: stockTicker,
+        Name: companyName,
+        Available_Shares: volume,
+        Price_Per_Share: initialPrice // Assuming your server expects this field for setting initial price
+    };
+    
+    // Make a fetch POST request to your server's endpoint for creating new stocks
+    fetch('http://54.176.181.88:3000/create_stock', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(stockData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        // You could refresh the stocks displayed or inform the user of success here
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 
     
