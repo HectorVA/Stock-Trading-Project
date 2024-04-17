0070a10e236474d771610a09d3157f771853da96
Hector Vazquez <57381072+HectorVA@users.noreply.github.com>
2024-04-17 12:42:08 -0700

    modified:   Java Files/home.js

--- Java Files/home.js
+++ Java Files/home.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Handle the case where there is no first name or last name in localStorage, maybe redirect to login
     }
     // Set the display content to username if available, otherwise use email
-    fullNameElement.textContent = firstName & lastName || userEmail; 
+    userNameElement.textContent = username || userEmail; 
 
     // Check if the stored user email indicates an admin user
     const isAdmin = localStorage.getItem('isAdmin') === 'true';
